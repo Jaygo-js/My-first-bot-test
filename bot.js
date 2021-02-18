34520155f98e22c20d5e6116e482b1a095e38105
Jaygo.js <jacobgriff123@gmail.com>
2021-02-18 02:05:21 +0000

    My first Bot test

---
+++ bot.js
@@ -0,0 +1,40 @@
+ require('dotenv').config()
+ 
+ const Discord = require('discord.js');
+ const client = new Discord.Client({
+     partials: ["MESSAGE"]
+ });
+ const BOT_PREFIX = '!'
+ const MOD_ME_COMMAND = 'mod-me'
+ const UNMOD_ME_COMMAND = 'unmod-me'
+ 
+ client.on('ready', () => {
+   console.log(`Logged in as ${client.user.tag}!`);
+ });
+ 
+client.on('messageDelete', msg => {
+    msg.channel.send("Stop deleting messages you rat!")
+})
+
+ client.on('message', msg => {
+     if(msg.content == 'gay'){
+         msg.reply('You are gay') && msg.react('👎')
+     }
+
+   if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+       modUser(msg.member)
+    }
+    if(msg.content === `${BOT_PREFIX}${UNMOD_ME_COMMAND}`){
+        unModUser(msg.member)
+    }
+ });
+ 
+function modUser(member){
+    member.roles.add("811772044148211743")
+}
+
+function unModUser(member){
+    member.roles.remove("811772044148211743")
+}
+
+ client.login(process.env.BOT_TOKEN);
